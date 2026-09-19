1c4785ed3dbf9733ef7e6cf440a441dc02459db2
golu380 <amitkumardubey744@gmail.com>
2026-09-19 15:43:30 +0530

    adding abstract class

---
+++ AbstractClass.java
@@ -0,0 +1,40 @@
+ abstract class Shape {
+
+    String color;
+    Shape( String color){
+        this.color= color;
+    }
+
+    abstract double area();
+    void  displayColor(){
+        System.out.println("color is: "+color);
+    }
+    
+}
+
+class Circle extends  Shape{
+
+    double radius;
+    Circle(double radius , String color){
+        super(color);
+        this.radius = radius;
+        
+    }
+
+    double area(){
+        return  Math.PI * radius * radius;
+    }
+}
+
+
+public class AbstractClass {
+    public static void  main(String [] args){
+
+        System.out.println("hii");
+        // Shape s1 = new Shape("blue"); we could not create objects for abstract class
+        Circle c1 = new Circle(7, "saffron");
+        // System.out.println(Math.PI);
+       System.out.println("the area of cicle is"+c1.area());
+       c1.displayColor();
+    }
+}
